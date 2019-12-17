8712b285d9dd4c357be2e5b63b4f38636f6ce462
Bethelhem-Gebre <bethelhemgebremariam@gmail.com>
2019-12-18 00:26:01 +0300

    Implmented review repo and service

---
+++ review/repository/gorm_review.go
@@ -0,0 +1,77 @@
+package repository
+
+import (
+	"github.com/jinzhu/gorm"
+	"xcut/entity"
+	"xcut/review"
+)
+
+type ReviewGormRepo struct {
+	conn *gorm.DB
+}
+
+
+// NewReviewtGormRepo returns new object of ReviewGormRepo
+func NewReviewGormRepo(db *gorm.DB) review.ReviewRepository {
+	return &ReviewGormRepo{conn: db}
+}
+
+//  GetReviews returns all reviews stored in the database
+func (revRepo *ReviewGormRepo) GetReviews() ([]entity.Review, []error) {
+	revws := []entity.Review{}
+	errs := revRepo.conn.Find(&revws).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return revws, errs
+}
+// GetReviewByShopID retrieve a review from the database by its shopid
+func (revRepo *ReviewGormRepo) GetReviewsByShopID(ShopID uint) ([]entity.Review, []error) {
+	revo := []entity.Review{}
+	errs := revRepo.conn.Set("gorm:auto_preload",true).Find(&revo, "shop_id=?",ShopID).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return revo, errs
+}
+// UpdateReview updates a given review in the database
+func (revRepo *ReviewGormRepo) UpdateReview(review *entity.Review) (*entity.Review, []error){
+	revi := review
+	errs := revRepo.conn.Save(revi).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return revi, errs
+}
+
+func (revRepo *ReviewGormRepo) GetReview(id uint) (*entity.Review, []error) {
+	review := entity.Review{}
+	errs := revRepo.conn.Set("gorm:auto_preload",true).First(&review,id).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return &review, errs
+}
+
+// DeleteReview deletes a given review from the database
+func (revRepo *ReviewGormRepo) DeleteReview(id uint) (*entity.Review, []error) {
+	review, errs := revRepo.GetReview(id)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	errs = revRepo.conn.Delete(review, review.ID).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return review, errs
+}
+
+// StoreReview stores a given review in the database
+func (revRepo *ReviewGormRepo) StoreReview(review *entity.Review) (*entity.Review, []error) {
+	revi := review
+	errs := revRepo.conn.Create(revi).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return revi, errs
+}
---
+++ review/services/review_service.go
@@ -0,0 +1,69 @@
+package services
+
+import (
+	"xcut/entity"
+	"xcut/review"
+)
+
+type ReviewService struct{
+	reviewRepo review.ReviewRepository
+}
+
+
+
+func NewReviewService(revoRepo review.ReviewRepository) review.ReviewService {
+	return &ReviewService{reviewRepo: revoRepo}
+}
+
+// GetReviews returns all stored reviews
+func (rs *ReviewService) GetReviews() ([]entity.Review, []error) {
+	revs, errs := rs.reviewRepo.GetReviews()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return revs, errs
+}
+
+func (rs *ReviewService) GetReview(id uint) (*entity.Review, []error) {
+	rev, errs := rs.reviewRepo.GetReview(id)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return rev, errs
+}
+
+
+// GetReviewByShopID  retrieves stored comment by its id
+func (rs *ReviewService) GetReviewsByShopID(ShopID uint) ([]entity.Review, []error) {
+	revs, errs := rs.reviewRepo.GetReviewsByShopID(ShopID)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return revs, errs
+}
+// UpdateReview updates a given review in a database
+func (rs *ReviewService) UpdateReview(review *entity.Review) (*entity.Review, []error) {
+	revs, errs := rs.reviewRepo.UpdateReview(review)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return revs, errs
+}
+// DeleteReview deletes a given review
+func (rs *ReviewService) DeleteReview(id uint) (*entity.Review, []error)  {
+	revs, errs := rs.reviewRepo.DeleteReview(id)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return revs, errs
+}
+
+
+// StoreReview stores a given review
+func (rs *ReviewService) StoreReview(review *entity.Review) (*entity.Review, []error) {
+	revs, errs := rs.reviewRepo.StoreReview(review)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return revs, errs
+}
