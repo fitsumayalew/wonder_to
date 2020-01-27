36287c4ade3669fd68259f289f8e65b48d621da0
Bethelhem-Gebre <bethelhemgebremariam84@gmail.com>
2020-01-27 17:00:07 +0300

    Made changes to entities

--- entity/entities.go
+++ entity/entities.go
@@ -14,44 +14,48 @@ type Service struct {
 	gorm.Model
 	Name          string `gorm:"type:varchar(255);not null"`
 	ShopID        uint
+	Shop          Shop
 	Price         float32
 	EstimatedTime uint
+	Image         string `gorm:"type:varchar(128);not null`
 }
 
 type User struct {
 	gorm.Model
-	FullName string `gorm:"type:varchar(255);not null"`
-	Email    string `gorm:"type:varchar(255);not null;unique"`
-	Password string `gorm:"type:varchar(255);not null"`
-	Phone    string `gorm:"type:varchar(16);not null; unique"`
-	RoleID   uint
+	FullName        string `gorm:"type:varchar(255);not null"`
+	Email           string `gorm:"type:varchar(255);not null;unique"`
+	Password        string `gorm:"type:varchar(255);not null"`
+	Phone           string `gorm:"type:varchar(16);not null; unique"`
+	Image           string `gorm:"type:varchar(128);`
+	NumberOfReviews uint
+	RoleID          uint
 }
 
 //Shop Represents the the shops the user views
 type Shop struct {
 	gorm.Model
-	Name      string `gorm:"type:varchar(255);not null"`
-	City      string
-	Lat       float64
-	Long      float64
-	Address   string `gorm:"type:varchar(255);not null; unique"`
-	Phone     string `gorm:"type:varchar(32)"`
-	Website   string `gorm:"type:varchar(255)"`
-	Image     string `gorm:"type:varchar(255)"`
-	UserID    uint
-	WeekDayOpenHour uint `gorm:"default:480"`
+	Name             string `gorm:"type:varchar(255);not null"`
+	City             string
+	Lat              float64
+	Long             float64
+	Address          string `gorm:"type:varchar(255);not null; unique"`
+	Phone            string `gorm:"type:varchar(32)"`
+	Website          string `gorm:"type:varchar(255)"`
+	Image            string `gorm:"type:varchar(255)"`
+	UserID           uint
+	WeekDayOpenHour  uint `gorm:"default:480"`
 	WeekDayCloseHour uint `gorm:"default:1200"`
-	WeekendOpenHour uint `gorm:"default:510"`
+	WeekendOpenHour  uint `gorm:"default:510"`
 	WeekendCloseHour uint `gorm:"default:1080"`
-	Services  []Service
 }
-
 
 type Appointment struct {
 	gorm.Model
-	UserID     uint
-	ShopID     uint
-	ServicesID uint
+	UserID          uint
+	ShopID          uint
+	ServicesID      uint
+	User            User
+	Shop            Shop
 	AppointmentTime *time.Time
 }
 
@@ -67,7 +71,9 @@ type Review struct {
 	gorm.Model
 	UserID uint
 	ShopID uint
-	Review string `gorm:"type:varchar(1024);not null"`
-	Reply  string `gorm:"type:varchar(1024);not null"`
+	User   User
+	Shop   Shop
+	Review string  `gorm:"type:varchar(1024);not null"`
+	Reply  string  `gorm:"type:varchar(1024);not null"`
 	Rating float32 `gorm:"not null"`
 }
