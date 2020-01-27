06b562bb75dc338db3e904ce57341910c571cfed
johnmerga <Johnmerga@yahoo.com>
2020-01-27 21:00:12 +0300

    Added constants file

--- constants/constants.go
+++ constants/constants.go
@@ -1,15 +1,32 @@
 package constants
 
-const fullnameKey = "fullname"
-const passwordKey = "password"
-const emailKey = "email"
-const phoneKey = "phone"
-const typeKey = "type"
-const confirmPasswordKey = "confirmPassword"
-const shopNameKey = "shopName"
-const addressKey = "address"
-const websiteKey = "website"
-const lngKey = "lng"
-const latKey = "lat"
-const cityKey = "city"
-const csrfKey = "_csrf"
+const FullnameKey = "fullname"
+const PasswordKey = "password"
+const EmailKey = "email"
+const PhoneKey = "phone"
+const TypeKey = "type"
+const ConfirmPasswordKey = "confirmPassword"
+const ShopNameKey = "shopName"
+const AddressKey = "address"
+const WebsiteKey = "website"
+const LngKey = "lng"
+const LatKey = "lat"
+const CityKey = "city"
+const CsrfKey = "_csrf"
+
+const ReplyKey = "reply_text"
+const ReviewIDKey = "review_id"
+
+const ServiceETCKey = "etc"
+const ServicePriceKey = "price"
+const ServiceNameKey = "name"
+
+const WeekdaysOpenHoursStart = "weekdays_start"
+const WeekdaysOpenHoursEnd = "weekdays_end"
+const WeekendsOpenHoursStart = "weekend_start"
+const WeekendsOpenHoursEnd = "weekend_end"
+
+
+const OpenHours = "openhours"
+
+const ImageFile = "image_file"
